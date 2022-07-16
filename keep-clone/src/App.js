bc7a2aca4c77420677f11895d36195da90081ffe
Naman-codes2001 <naman.deep991@gmail.com>
2022-07-17 00:50:47 +0530

    Google-Keep-Clone App is made using React.

---
+++ keep-clone/src/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import "./styles.css";
+import Header from "./components/Header";
+import CreateArea from "./components/CreateArea";
+import Note from "./components/Note";
+import Count from "./components/Count";
+import Footer from "./components/Footer";
+
+function App(props) {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes((prevValue) => {
+      return [...prevValue, newNote];
+    });
+  }
+
+  function deleteNotes(id) {
+    setNotes((preValue) => {
+      return [...preValue.filter((note, index) => index !== id)];
+    });
+  }
+
+  return (
+    <div className="App">
+      <Header />
+      <Count
+        count={
+          notes.length === 0
+            ? "No Note in Database"
+            : `Showing ${notes.length} Notes in Database`
+        }
+      />
+      <CreateArea onAdd={addNote} />
+      {notes.map((note, index) => (
+        <Note
+          key={index}
+          id={index}
+          title={note.title}
+          content={note.content}
+          onDelete={deleteNotes}
+        />
+      ))}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ keep-clone/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer(props) {
+  return (
+    <div className="footer">
+      <p>Copyright Ⓒ 2022</p>
+    </div>
+  );
+}
+
+export default Footer;
